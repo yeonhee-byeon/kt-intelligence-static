f9174c8e51f3663f94a10a9e382f17e201e3b66c
mwHyung <mw.hyung@eluocnc.com>
2025-06-06 10:46:09 +0900

    250606_서브 이름, 이미지 등 수정 반영 및 경로 수정

--- js/ai/solution.js
+++ js/ai/solution.js
@@ -1,56 +1,56 @@
-const bannerSection = document.querySelector("#banner-section");
-// const subBannerSection = document.querySelector("#sub-banner-section");
-if (bannerSection) {
-    const header = document.querySelector("#main-header");
-    const observer = new IntersectionObserver(
-        (entries) => {
-            entries.forEach((entry) => {
-                if (entry.isIntersecting) {
-                    header.classList.add("dark-header");
-                } else {
-                    header.classList.remove("dark-header");
-                }
-            });
-        },
-        {
-            threshold: 0.1,
-        }
-    );
+// const bannerSection = document.querySelector("#banner-section");
+// // const subBannerSection = document.querySelector("#sub-banner-section");
+// if (bannerSection) {
+//     const header = document.querySelector("#main-header");
+//     const observer = new IntersectionObserver(
+//         (entries) => {
+//             entries.forEach((entry) => {
+//                 if (entry.isIntersecting) {
+//                     header.classList.add("dark-header");
+//                 } else {
+//                     header.classList.remove("dark-header");
+//                 }
+//             });
+//         },
+//         {
+//             threshold: 0.1,
+//         }
+//     );
 
-    observer.observe(bannerSection);
-}
+//     observer.observe(bannerSection);
+// }
 
-function checkHeader() {
-    const header = document.querySelector("#main-header");
-    const contentSection = document.querySelector(".content-wrapper");
-    const contentSectionRect = contentSection.getBoundingClientRect();
+// function checkHeader() {
+//     const header = document.querySelector("#main-header");
+//     const contentSection = document.querySelector(".content-wrapper");
+//     const contentSectionRect = contentSection.getBoundingClientRect();
 
-    if (contentSectionRect.top < 0) {
-        header.classList.remove("dark-header");
-    } else {
-        header.classList.add("dark-header");
-    }
-}
+//     if (contentSectionRect.top < 0) {
+//         header.classList.remove("dark-header");
+//     } else {
+//         header.classList.add("dark-header");
+//     }
+// }
 
-window.addEventListener("scroll", checkHeader);
+// window.addEventListener("scroll", checkHeader);
 
 const cardListHorizontal = document.querySelector(".card-list-horizontal.swiper-container");
 function initMobileSwiper() {
-    if (window.innerWidth < 768) {
-        if (!window.mobileSwiper) {
-            window.mobileSwiper = new Swiper(".card-list-horizontal.swiper-container", {
-                slidesPerView: 1.2,
-                spaceBetween: 16,
-            });
-        }
-    } else {
-        if (window.mobileSwiper) {
-            window.mobileSwiper.destroy();
-            window.mobileSwiper = null;
-        }
+  if (window.innerWidth < 768) {
+    if (!window.mobileSwiper) {
+      window.mobileSwiper = new Swiper(".card-list-horizontal.swiper-container", {
+        slidesPerView: 1.2,
+        spaceBetween: 16,
+      });
     }
+  } else {
+    if (window.mobileSwiper) {
+      window.mobileSwiper.destroy();
+      window.mobileSwiper = null;
+    }
+  }
 }
 if (cardListHorizontal) {
-    window.addEventListener("resize", initMobileSwiper);
-    window.addEventListener("DOMContentLoaded", initMobileSwiper);
+  window.addEventListener("resize", initMobileSwiper);
+  window.addEventListener("DOMContentLoaded", initMobileSwiper);
 }
