03f4915d79275328e3d4ef8d42d91eb6c5c7209a
mwHyung <mw.hyung@eluocnc.com>
2025-06-12 17:25:31 +0900

    RAG,RAI PC 수정반영

--- js/ai/solution.js
+++ js/ai/solution.js
@@ -20,45 +20,45 @@
 //     observer.observe(bannerSection);
 // }
 
-function checkHeader({ isDark = false, target = "" } = {}) {
-  if (!target) return;
-  const header = document.querySelector("#main-header");
-  const contentSection = document.querySelector(target);
-  const observer = new IntersectionObserver(
-    (entries) => {
-      entries.forEach((entry) => {
-        if (entry.isIntersecting || isDark) {
-          header.classList.add("dark-header");
-        } else {
-          header.classList.remove("dark-header");
-        }
-      });
-    },
-    {
-      threshold: 0.1,
-    }
-  );
+function checkHeader({ isDark = false, target = '' } = {}) {
+    if (!target) return;
+    const header = document.querySelector('#main-header');
+    const contentSection = document.querySelector(target);
+    const observer = new IntersectionObserver(
+        (entries) => {
+            entries.forEach((entry) => {
+                if (entry.isIntersecting || isDark) {
+                    header.classList.add('dark-header');
+                } else {
+                    header.classList.remove('dark-header');
+                }
+            });
+        },
+        {
+            threshold: 0.1,
+        },
+    );
 
-  observer.observe(contentSection);
+    observer.observe(contentSection);
 }
 
-const cardListHorizontal = document.querySelector(".card-list-horizontal.swiper-container");
+const cardListHorizontal = document.querySelector('.card-list-horizontal.swiper-container');
 function initMobileSwiper() {
-  if (window.innerWidth < 768) {
-    if (!window.mobileSwiper) {
-      window.mobileSwiper = new Swiper(".card-list-horizontal.swiper-container", {
-        slidesPerView: 1.2,
-        spaceBetween: 16,
-      });
+    if (window.innerWidth < 768) {
+        if (!window.mobileSwiper) {
+            window.mobileSwiper = new Swiper('.card-list-horizontal.swiper-container', {
+                slidesPerView: 1.2,
+                spaceBetween: 16,
+            });
+        }
+    } else {
+        if (window.mobileSwiper) {
+            window.mobileSwiper.destroy();
+            window.mobileSwiper = null;
+        }
     }
-  } else {
-    if (window.mobileSwiper) {
-      window.mobileSwiper.destroy();
-      window.mobileSwiper = null;
-    }
-  }
 }
 if (cardListHorizontal) {
-  window.addEventListener("resize", initMobileSwiper);
-  window.addEventListener("DOMContentLoaded", initMobileSwiper);
+    window.addEventListener('resize', initMobileSwiper);
+    window.addEventListener('DOMContentLoaded', initMobileSwiper);
 }
